5e25f39c498204ec1b3da29b0d1ba55f9edcf7b0
HernanGarcia1985 <hernang1985@gmail.com>
2022-02-28 15:53:20 -0300

    Pelicula Router y Controller

--- src/controllers/peliculaController.js
+++ src/controllers/peliculaController.js
@@ -1,4 +1,163 @@
+const db = require('../database/models');
+const Pelicula = require('../database/models/pelicula');
+
 const peliculaController = {
+
+    todasLasPeliculas: (req, res) => {
+		let resultado = {
+			link: "http://localhost:3000/movies",
+			cantidad: 0,
+			data: []
+		}
+
+		db.Pelicula.findAll({
+            attributes: ['Imagen','Titulo','FechaCreacion']})
+				.then((totalDePeliculas) => {
+					if(totalDePeliculas){
+						resultado.data = totalDePeliculas;
+						resultado.cantidad = totalDePeliculas.length;
+						res.json(resultado);
+					}
+				})
+				.catch(function(error){
+					console.log("No se pudo acceder a la base de datos");
+				})
+		},
+
+        peliculaPorId: (req, res) => {
+			let resultado = {
+				link: "http://localhost:3000/movies/:id",
+				cantidad: 0,
+				data: []
+			}
+            let promesaPelicula = db.Pelicula.findByPk(req.params.id);
+            let promesaPersonaje = db.Personaje.findAll({
+                attributes: ['Nombre'],
+                where:{Pelicula_id:req.params.id},
+                include:[{association:'personajes'}]
+            });
+
+			Promise.all([promesaPelicula,promesaPersonaje])
+				.then(function([resultadoPelicula,resultadoPersonaje]){
+					if(resultadoPelicula){
+						resultado.data.push(resultadoPelicula.dataValues);
+                        resultado.cantidad = 1;
+                        if(resultadoPersonaje){    
+                            resultado.data.push(resultadoPersonaje.dataValues);	
+                        }
+                    }
+					res.json(resultado);
+				})
+				.catch(function(error){
+					console.log("No se pudo acceder a la base de datos");
+				})
+		},
+
+        peliculaCreate: (req, res) => {
+
+            db.Pelicula.findOne( //Buscar pelicula por titulo y si no esta crearla
+                {
+                    where: {Titulo:req.body.titulo}
+                }
+            )
+            .then((resultado)=>{
+                console.log(resultado);
+                res.redirect('/movies');
+            })
+            .catch(function(error){
+                db.Pelicula.create({
+                    Imagen: "/images/" + req.file.filename,
+                    Titulo: req.body.titulo ,
+                    FechaCreacion: req.body.fechaCreacion,
+                    Calificacion: req.body.calificacion,
+                    Genero_id: req.body.genero,
+                });
+                console.log("Se creó pelicula");
+                res.redirect('/movies');
+            })
+        },
+
+        peliculaUpdate: (req, res) => {
+
+            db.Pelicula.update(
+                {Imagen: "/images/" + req.file.filename,
+                Titulo: req.body.titulo ,
+                FechaCreacion: req.body.fechaCreacion,
+                Calificacion: req.body.calificacion,
+                Genero_id: req.body.genero
+                },
+                {where: {id:req.params.id}}
+            );
+
+            res.redirect('/movies');
+        },
+
+        peliculaDestroy: (req, res) => {
+
+            db.Pelicula.findByPk(req.params.id)
+			.then((pelicula) =>{
+				fs.unlinkSync(path.join(__dirname, '../../public/images/', pelicula.Imagen));
+			})
+			.catch(function(error){
+				console.log("error!");
+			})
+
+            db.Pelicula.destroy({
+                where: {
+                    id:req.params.id
+                }
+            });
+
+            res.redirect('/movies');
+        },
+
+        peliculasPorTitulo: (req, res) => {     //Instalar Op y buscar con Like
+			let resultado = {
+				link: "http://localhost:3000/movies?name",
+				cantidad: 0,
+				data: []
+			}
+
+            db.Pelicula.findAll({
+                where:{Titulo:req.query.name},
+                order:[['Titulo',req.query.order]]
+            })
+            .then((peliculas) => {
+                if(peliculas){
+                    resultado.data = peliculas; 
+                    resultado.cantidad = peliculas.length;
+                    res.json(resultado);
+                }
+            })
+            .catch(function(error){
+                console.log("No se pudo acceder a la base de datos");
+            })
+        },
+
+        peliculasPorGeneroId: (req, res) => {
+			let resultado = {
+				link: "http://localhost:3000/movies?genre",
+				cantidad: 0,
+				data: []
+			}
+
+            db.Pelicula.findAll({
+                attributes: ['Imagen','Titulo','FechaCreacion','Calificacion','Nombre'],
+                where:{Genero_id:req.query.genre},
+                order:[['Genero_Id',req.query.order]],
+                include:[{association:'generos'}]
+            })
+            .then((peliculas) => {
+                if(peliculas){
+                    resultado.data = peliculas; 
+                    resultado.cantidad = peliculas.length;
+                    res.json(resultado);
+                }
+            })
+            .catch(function(error){
+                console.log("No se pudo acceder a la base de datos");
+            })
+        }
 
 }
 
--- src/routes/peliculaRouter.js
+++ src/routes/peliculaRouter.js
@@ -1,2 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const peliculaController = require('../controllers/peliculaController');
+var cors = require('cors');
+
+
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+router.get('/movies',cors(corsOptions),peliculaController.todasLasPeliculas);
+
+router.get('/movies/:id',cors(corsOptions),peliculaController.peliculaPorId);
+
+router.post('/movies',cors(corsOptions),peliculaController.peliculaCreate);
+
+router.put('/movies/:id',cors(corsOptions),peliculaController.peliculaUpdate);
+
+router.delete('/movies/:id',cors(corsOptions),peliculaController.peliculaDestroy);
+
+router.get('/movies?name',cors(corsOptions),peliculaController.peliculasPorTitulo);
+
+router.get('/movies?genre',cors(corsOptions),peliculaController.peliculasPorGeneroId);
