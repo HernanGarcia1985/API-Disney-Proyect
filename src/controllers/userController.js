7c2540c415313fad9aa705caf2b89bda5dffcded
HernanGarcia1985 <hernang1985@gmail.com>
2022-02-28 17:36:58 -0300

    Usuario Router y Controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,89 @@
-const userController = {
+const db = require('../database/models');
+const Usuario = require('../database/models/usuario');
+const bcryptjs = require('bcryptjs');
+const jwt = require("jsonwebtoken");
 
+const secretKey = 'Mi Llave Ultra Secreta' // Agregarla al ENV
+
+const userController = {
+    login: (req, res) => {
+
+        let resultado = {
+			link: "http://localhost:3000/auth/login",
+			cantidad: 0,
+			data: [],
+            token: []
+		}
+
+        db.Usuario.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+            .then((usuarioLogin) =>{
+                if(usuarioLogin) {
+                    let validarPassword = bcryptjs.compareSync(req.body.password, usuarioLogin.password);
+                    if (validarPassword == true) { 
+                        const token = jwt.sign(
+                            {
+                            exp: Math.floor(Date.now() / 1000) + 1200, //20 min
+                            data: usuarioLogin,
+                            },
+                            secretKey
+                            );
+
+                        resultado.data.push("Bienvenido, " + req.body.email);
+                        resultado.cantidad = 1;
+                        resultado.token.push(token);
+                        return res.json(resultado);
+                    } 
+                    else{
+                        resultado.data.push('Las credenciales son inválidas');
+                        return res.json(resultado);   
+                    }
+                }
+                else{
+                    resultado.data.push('No se encuentra este email en nuestra base de datos');
+                    return res.json(resultado);
+                } 
+            })
+            .catch(function(error){
+                console.log("No se encuentra este email en nuestra base de datos");
+            })
+    },
+
+    register: (req, res) => {
+
+        let resultado = {
+			link: "http://localhost:3000/auth/register",
+			cantidad: 0,
+			data: [],
+		}
+
+        db.Usuario.findOne({
+			where: {
+				email: req.body.email
+			}
+		})
+			.then((usuarioEnBD) =>{
+				if (usuarioEnBD) {
+                    resultado.data.push('Este email ya está registrado en nuestra base de datos');
+                    return res.json(resultado);
+				}
+                else{
+                    db.Usuario.create({
+                        email: req.body.email,
+                        password: bcryptjs.hashSync(req.body.password, 10),
+                    });
+                    resultado.data.push('Su email fue registrado en nuestra base de datos');
+                    resultado.cantidad = 1;
+                    return res.json(resultado);
+                }
+			})
+			.catch(function(error){
+				console.log("No se pudo crear el registro en la base de datos");
+			})
+		}
 }
 
 module.exports = userController;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/userController');
+var cors = require('cors');
+
+
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/auth/login',cors(corsOptions),userController.login);
+
+router.post('/auth/register',cors(corsOptions),userController.register);
 
 module.exports = router;
